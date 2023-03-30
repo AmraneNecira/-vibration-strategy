eadfebd4099ef12f7f2bbe288ba478cd786339f7
Amrane Necira <93205157+AmraneNecira@users.noreply.github.com>
2023-03-30 17:05:04 +0000

    add Matrix HAL headers to control Matrix GPIOs

--- main.cpp
+++ main.cpp
@@ -2,19 +2,33 @@
 #include <unistd.h>
 #include <chrono>
 #include "vib.h"
+#include "matrix_hal/gpio_control.h"
+#include "matrix_hal/matrixio_bus.h"
 
 using namespace std::chrono;
 
+/**GPIO setup*/
+const uint16_t GPIOOutputMode = 1;
+matrix_hal::GPIOControl gpio;
+uint16_t pin_out ;
+
+/**Vibrator Selector*/
+//TODO: Which Vibrator Vibs!
+
 /**Vib Function */
-void vib(float level){
+void vib(uint16_t pin_out, float level){
     int limit = (int) NbrVibSeg(level)-1;
     auto start = high_resolution_clock::now();
+    gpio.SetMode(pin_out, GPIOOutputMode);// set pin_out mode to 1 
+
     for (int i = 0; i < TOTAL_SEG; ++i) {
         if (i <= limit ){
-            printf("ON for itr %d with level %f\n", i, level);
+            gpio.SetGPIOValue(pin_out,1);
+            printf("ON for itr %d with level %f vibrator %d\n", i, level, pin_out);
             usleep(SEG_SIZE);
         }
         else {
+            gpio.SetGPIOValue(pin_out,0);
             printf("OFF for itr %d \n", i);
             usleep(SEG_SIZE);
         }
@@ -26,13 +40,23 @@ void vib(float level){
 }
 
 int main() {
-    float niv;
+    // Hardware Communication through the matrix GPIO
+    matrix_hal::MatrixIOBus bus;
+    if (!bus.Init()) return false;
+    gpio.Setup(&bus);
+    
+    // Pin out number
+    std::cout << "Select pin_out from [0-15]: ";
+    std::cin >> pin_out;
+
     // input the level
+    float niv;
+    
     while (true) {
         std::cout << "Select Level of vibration: ";
         std::cin >> niv;
         std::cout << "total segments " << TOTAL_SEG << std::endl;
         std::cout << "Nbr of Vib. segments : " << NbrVibSeg(niv) << std::endl;
-        vib(adjustLevel(niv));
+        vib(pin_out, adjustLevel(niv));
     }
 }
